d73c16953ae0cc5df445382f09e99b855a75b7ce
Ignacio Pinto Rodriguez <tripleignacio@gmail.com>
2023-05-01 22:30:32 +0100

    read images cv2

--- readImages.py
+++ readImages.py
@@ -1,18 +1,12 @@
 import cv2
 import pytesseract
 pytesseract.pytesseract.tesseract_cmd = "C:/Program Files/Tesseract-OCR/tesseract.exe"
-image = cv2.imread('static/boleto.jpg')
+image = cv2.imread('static/euromillones.png')
 #read numbers delete background image add blur and scales white and black
 image2 = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
 image2 = cv2.threshold(image2,0,255,cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
 image2 = cv2.medianBlur(image2,3)
 image2 = cv2.resize(image2,None,fx=2,fy=2,interpolation=cv2.INTER_CUBIC)
-
-
-
-
-
-
 
 text = pytesseract.image_to_string(image2,lang='spa')
 print('Texto: ',text)
